a297bf66b07d056bc0333f23b5d88ff38e06115f
Gandharva Gandharva <gg297@afsaccess1.njit.edu>
2017-12-06 18:24:28 -0500

    registeration form update

--- register.php
+++ register.php
@@ -5,6 +5,10 @@ Username : <input type="text" placeholder="Username" name="username" required />
 Email : <input type="email" placeholder="Email" name="email" required /><br><br>
 First Name : <input type="text" placeholder="Firstname" name="firstname" required /><br><br>
 Last Name : <input type="text" placeholder="Lastname" name="lastmname" required/><br><br>
+Gender : <input type="radio"  name="gender" value="Female">Female
+         <input type="radio" name="Gender" value="male">Male<br><br>   
+ph No.: <input type="text" placeholder="phonenumber" name="phonenumber" required/><br><br>
+D.O.B : <input type="text" placeholder="birthdate" name="birthdate" required/><br><br>
 Password : <input type="password" placeholder="Password" name="password" required /><br><br>
 Confirm Password : <input type="password" placeholder="Confirm Password" name="confirm password" required /><br><br>
 <input type="submit" value="Register">
