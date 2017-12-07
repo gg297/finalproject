8702b28b4dd3e9464f9a21250a6b19c0f3c01710
Gandharva Gandharva <gg297@afsaccess1.njit.edu>
2017-12-06 19:02:39 -0500

    login page

---
+++ login.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<p><b><h2>LogIn</h2></b></p>
+<body>
+<form action="/action_page.php" method="post" encript="multipart/form-data">
+
+Email : <input type="email" placeholder="Email" name="email" required /><br><br>
+First Name : <input type="text" placeholder="Firstname" name="firstname" required /><br><br>
+Last Name : <input type="text" placeholder="Lastname" name="lastmname" required/><br><br>
+Password : <input type="password" placeholder="password" name="psw" required/><br><br>
+<input type="submit" value="login">
+<p> Not Registered? New user? </p> <a href="register.php"><h2>SignUP</h2></a>
+</form>
+</body>
+</html>
